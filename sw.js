99049466cd7e8df176eefe0d2d435c476ca364d4
Stanislav <83785390+FootballMafia@users.noreply.github.com>
2026-02-08 02:28:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,14 +16,7 @@ self.addEventListener("fetch", e => {
     })
   );
 });
-let deferredPrompt;
 
-window.addEventListener("beforeinstallprompt", e => {
-  e.preventDefault();
-  deferredPrompt = e;
-  document.getElementById("installBtn").style.display = "block";
-});
-
-document.getElementById("installBtn").addEventListener("click", () => {
-  deferredPrompt.prompt();
-});
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
